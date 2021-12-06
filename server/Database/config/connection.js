c3e147500e0640a2c4888ed1fa3edc94508623fc
linaYahya <linaebe0@gmail.com>
2021-12-06 11:13:31 +0200

    ssl connection switched

--- server/Database/config/connection.js
+++ server/Database/config/connection.js
@@ -2,20 +2,19 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 let dbUrl = '';
+const { NODE_ENV, DB_URL_TEST, DB_URL_DEV, DATABASE_URL } = process.env
 
-// if(!dbUrl) throw new
-
-if (process.env.NODE_ENV === 'test') {
-  dbUrl = process.env.DB_URL_TEST;
-} else if (process.env.NODE_ENV === 'development') {
-  dbUrl = process.env.DB_URL_DEV;
+if (NODE_ENV === 'test') {
+  dbUrl = DB_URL_TEST;
+} else if (NODE_ENV === 'development') {
+  dbUrl = DB_URL_DEV;
 } else {
-  dbUrl = process.env.DATABASE_URL;
+  dbUrl = DATABASE_URL;
 }
 console.log(dbUrl, 'connection')
 const option = {
   connectionString: dbUrl,
-  ssl: { rejectUnauthorized: false },
+  ssl: NODE_ENV === "test" ? false :{ rejectUnauthorized: false },
 };
 
 module.exports = new Pool(option);
